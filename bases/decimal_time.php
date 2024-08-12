b6f43a33742a26d052b126a3204110246f5ee6de
Fonzie-byte <twistfons@live.nl>
2024-08-13 00:20:26 +0200

    I'm sure I already started some UUID in Laravel benchmark but here goes

--- bases/decimal_time.php
+++ bases/decimal_time.php
@@ -3,7 +3,6 @@
 $decimalHours = intval(date('H')/2.4);
 
 $percentageOfHour = date('i')/60 + date('s')/3600;
-var_dump($percentageOfHour);
 $afterFloatingPoint = explode('.', $percentageOfHour)[1];
 $decimalMinutes = substr($afterFloatingPoint, 0, 2);
 $decimalSeconds = substr($afterFloatingPoint, 2, 2) ?? '00';
